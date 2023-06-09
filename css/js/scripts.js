689e8b05a3bbd50766c176fafdaabad00ab6ff87
veganpowerlifter <katya@total>
2023-06-09 09:01:18 -0400

    week 4 Scss changes

---
+++ css/js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000, pause: "false" } );
+    $("#carouselButton").click(function(){ <!--adding carousel controls, making play and pause switch-->
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
